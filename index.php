c05405d04baa64ad484408abe9fabb979e09095b
deguilardi <demetrio.guilardi@gmail.com>
2019-11-11 23:04:00 -0500

    now using bootstrap and jquery

--- index.php
+++ index.php
@@ -30,8 +30,6 @@ $reposOptions = array(
 	new RepositoryEntity( "/PierfrancescoSoffritti/android-youtube-player", GIT_BRANCH_DEFAULT, "" ), 
 	new RepositoryEntity( "/DrKLO/Telegram", GIT_BRANCH_DEFAULT, "" ), 
 
-	// @TODO different pattern
-
 	// work with no result
 	// new RepositoryEntity( "/android/topeka", GIT_BRANCH_DEFAULT, "" ),
 	// new RepositoryEntity( "/android/storage-samples", GIT_BRANCH_DEFAULT, "ActionOpenDocument" ),
@@ -60,6 +58,8 @@ $colors = array(
 
 $projects = $_POST[ "projects" ];
 $granulatity = $_POST[ "granulatity" ];
+$propToExtract = $_POST[ "propToExtract" ];
+define( 'PARAM_TO_EXTRACT', $propToExtract );
 
 
 function getPeriodWithGranulatity( $value, $granulatity ){
@@ -155,110 +155,194 @@ if( @sizeof( $projects) ){
 <html>
 <head>
 	<title>Android Project Prop Tracker</title>
+	<script   src="https://code.jquery.com/jquery-3.4.1.min.js"   integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo="   crossorigin="anonymous"></script>
 	<script src="https://cdn.jsdelivr.net/npm/chart.js@2.8.0"></script>
-	<style>
-	canvas {
-		-moz-user-select: none;
-		-webkit-user-select: none;
-		-ms-user-select: none;
-	}
-	</style>
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+	<link rel="stylesheet" href="./style.css" />
 </head>
 <body>
-<h1>Android Project Prop Tracker</h1>
-<form method="post">
-	<h3>Tested projects:</h3>
-	<select multiple name="projects[]" size="<?=sizeof($reposOptions);?>">
-		<?php
-		foreach( $reposOptions as $repo ){ 
-			$value = $repo->repo . ":" . $repo->branch . ":" . $repo->folder;
-		?>
-			<option value="<?=$value;?>"><?=$value;?></option>
-		<?php } ?>
-	</select>
-	<br/>
-	<h3>Granularity</h3>
-	<select name="granulatity">
-		<option value="quartely">Quartely</option>
-		<option value="monthly">Monthly</option>
-	</select>
-	<br />
-	<br />
-	<input type="submit" value="submit" />
-</form>
 
-<? if( @sizeof( $projects) ){ ?>
+<div class="container">
+	<h1>Android Project Prop Tracker</h1>
 
-<h2>Results</h2>
-<div style="width:90%; margin: 5%;">
-	<canvas id="canvas"></canvas>
+	<div class="accordion" id="accordionExample">
+		<div class="card">
+			<div class="card-header" id="headingOne">
+				<h2 class="mb-0">
+					<button class="btn btn-link" type="button" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">Form</button>
+				</h2>
+			</div>
+			<div id="collapseOne" class="collapse <?=( @sizeof( $projects) ) ? "" : "show";?>" aria-labelledby="headingOne" data-parent="#accordionExample">
+				<div class="card-body">
+
+					<form method="post">
+						<hr />
+						<div class="row">
+							<div class="col-sm">
+								<label for="preTestedProjects">Pre tested projects</label>
+								<select multiple class="form-control" name="projects[]" size="10" id="preTestedProjects">
+									<?php
+									foreach( $reposOptions as $repo ){ 
+										$value = $repo->repo . ":" . $repo->branch . ":" . $repo->folder;
+									?>
+										<option value="<?=$value;?>"><?=$value;?></option>
+									<?php } ?>
+								</select>
+							</div>
+							<div class="col-sm">
+								<label for="otherProjects">Other projects</label>
+								<textarea class="form-control" rows="8" id="otherProjects" placeholder="@todo" disabled></textarea>
+							</div>
+						</div>
+						<hr />
+						<div class="row">
+							<div class="col-sm">
+								<label for="propToExtract">Property to extract</label>
+								<div class="form-check">
+								  <input class="form-check-input" type="radio" name="propToExtract" id="minSdkVersion" value="minSdkVersion" >
+								  <label class="form-check-label" for="minSdkVersion">minSdkVersion</label>
+								</div>
+								<div class="form-check">
+								  <input class="form-check-input" type="radio" name="propToExtract" id="compileSdkVersion" value="compileSdkVersion" >
+								  <label class="form-check-label" for="compileSdkVersion">compileSdkVersion</label>
+								</div>
+								<div class="form-check">
+								  <input class="form-check-input" type="radio" name="propToExtract" id="targetSdkVersion" value="targetSdkVersion" checked>
+								  <label class="form-check-label" for="targetSdkVersion">targetSdkVersion</label>
+								</div>
+							</div>
+							<div class="col-sm">
+								<label for="granulatity">Granulatity</label>
+								<div class="form-check">
+								  <input class="form-check-input" type="radio" name="granulatity" id="granulatityMonthly" value="monthly" checked>
+								  <label class="form-check-label" for="granulatityMonthly">monthly</label>
+								</div>
+								<div class="form-check">
+								  <input class="form-check-input" type="radio" name="granulatity" id="granulatityQuartely" value="quartely">
+								  <label class="form-check-label" for="granulatityQuartely">quartely</label>
+								</div>
+							</div>
+						</div>
+						<hr />
+						<button type="submit" class="btn btn-primary">Submit</button>
+					</form>
+
+	      		</div>
+	    	</div>
+	  	</div>
+	  	<div class="card">
+		    <div class="card-header" id="headingTwo">
+			    <h2 class="mb-0">
+			        <button class="btn btn-link collapsed <?=( @sizeof( $projects) ) ? "" : "disabled";?>" type="button" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">Results</button>
+			    </h2>
+		    </div>
+		    <div id="collapseTwo" class="collapse <?=( @sizeof( $projects) ) ? "show" : "";?>" aria-labelledby="headingTwo" data-parent="#accordionExample">
+		    	<div class="card-body">
+		    		
+					<? if( @sizeof( $projects) ){ ?>
+						
+					<div class="resultsTableHolder">
+						<table class="resultsTable">
+							<thead>
+								<tr>
+									<th>API levels</th>
+									<?php foreach( $resultsByPeriod as $period => $results ){ ?>
+										<th><?=$period;?></th>
+									<?php } ?>
+								</tr>
+							</thead>
+							<tbody>
+								<?php foreach( $resultsByValue as $value => $results ){ ?>
+									<tr>
+									<td><?=$value;?></td>
+										<? foreach( $results as $result ){ ?>
+											<td><?=$result;?></td>
+										<? } ?>
+									</tr>
+								<?php } ?>
+							</tbody>
+						</table>
+					</div>
+
+					<div style="width:98%; margin: 1%;">
+						<canvas id="canvas"></canvas>
+					</div>
+
+					<script>
+					var lineChartData = {
+						labels: [
+							<?php
+							foreach( $resultsByPeriod as $period => $results ){
+								echo '"' . $period . '",';
+							}
+							?>
+						],
+						datasets: [
+							<?php
+							$i = 0;
+							foreach( $resultsByValue as $value => $results ){
+								echo "{
+									     label:'".$value."',
+								         borderColor: \"rgb(".$colors[ $i ].")\",
+								         backgroundColor: \"rgb(".$colors[ $i ].")\",
+								         fill:false,
+								         data:[";
+
+								foreach( $results as $result ){
+									echo ( $result ? $result : "null" ) . ",";
+								}
+								echo "]},";
+								$i++;
+							}
+							?>]
+					};
+
+					window.onload = function() {
+						var ctx = document.getElementById('canvas').getContext('2d');
+						window.myLine = Chart.Line(ctx, {
+							data: lineChartData,
+							options: {
+								responsive: true,
+								hoverMode: 'index',
+								stacked: false,
+								title: {
+									display: true,
+									text: 'Versions updates x Time'
+								},
+								scales: {
+									yAxes: [{
+										type: 'linear', // only linear but allow scale type registration. This allows extensions to exist solely for log scale for instance
+										display: true,
+										position: 'left',
+										id: 'y-axis-1',
+									}, {
+										type: 'linear', // only linear but allow scale type registration. This allows extensions to exist solely for log scale for instance
+										display: true,
+										position: 'right',
+										id: 'y-axis-2',
+
+										// grid line settings
+										gridLines: {
+											drawOnChartArea: false, // only want the grid lines for one axis to show up
+										},
+									}],
+								}
+							}
+						});
+					};
+					</script>
+
+					<? } ?>
+
+		    	</div>
+		    </div>
+		  </div>
+	</div>
+
+
 </div>
 
-<script>
-var lineChartData = {
-	labels: [
-		<?php
-		foreach( $resultsByPeriod as $period => $result ){
-			echo '"' . $period . '",';
-		}
-		?>
-	],
-	datasets: [
-		<?php
-		$i = 0;
-		foreach( $resultsByValue as $period => $results ){
-			echo "{
-				     label:'".$period."',
-			         borderColor: \"rgb(".$colors[ $i ].")\",
-			         backgroundColor: \"rgb(".$colors[ $i ].")\",
-			         fill:false,
-			         data:[";
-
-			foreach( $results as $result ){
-				echo ( $result ? $result : "null" ) . ",";
-			}
-			echo "]},";
-			$i++;
-		}
-		?>]
-};
-
-window.onload = function() {
-	var ctx = document.getElementById('canvas').getContext('2d');
-	window.myLine = Chart.Line(ctx, {
-		data: lineChartData,
-		options: {
-			responsive: true,
-			hoverMode: 'index',
-			stacked: false,
-			title: {
-				display: true,
-				text: 'Versions updates x Time'
-			},
-			scales: {
-				yAxes: [{
-					type: 'linear', // only linear but allow scale type registration. This allows extensions to exist solely for log scale for instance
-					display: true,
-					position: 'left',
-					id: 'y-axis-1',
-				}, {
-					type: 'linear', // only linear but allow scale type registration. This allows extensions to exist solely for log scale for instance
-					display: true,
-					position: 'right',
-					id: 'y-axis-2',
-
-					// grid line settings
-					gridLines: {
-						drawOnChartArea: false, // only want the grid lines for one axis to show up
-					},
-				}],
-			}
-		}
-	});
-};
-</script>
-
-<? } ?>
 
 </body>
 </html>
